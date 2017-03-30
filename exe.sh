3f174142ee32710886735ceb4e9cf22ae557d76e
Salvatore Davide Porzio <sporzio@uboonegpvm05.fnal.gov>
2017-03-30 17:08:14 -0500

    adding timestamp information for parallel jobs

--- exe.sh
+++ exe.sh
@@ -1,12 +1,16 @@
 # Path to script directory
-SCRIPTDIR="$(dirname "${BASH_SOURCE[0]}")"
+SCRIPTDIR=$(dirname "${BASH_SOURCE[0]}")
+TIMESTAMP=$(date +%s)
+
+mkdir ${SCRIPTDIR}/Logs/${TIMESTAMP}
+mkdir ${SCRIPTDIR}/Scripts/${TIMESTAMP}
 # Check if input is file, if not, run as string
 if [ -f "${1}" ]; then
-  python ${SCRIPTDIR}/generateScript.py -if ${PWD}/${1}
+  python ${SCRIPTDIR}/generateScript.py -if ${PWD}/${1} -t ${TIMESTAMP}
 else
-  python ${SCRIPTDIR}/generateScript.py -is "${1}"
+  python ${SCRIPTDIR}/generateScript.py -is "${1}" -t ${TIMESTAMP}
 fi
 
-source ${SCRIPTDIR}/script.tempexe
-rm ${SCRIPTDIR}/script.tempexe
-rm ${SCRIPTDIR}/Logs/*.templog
+source ${SCRIPTDIR}/Scripts/${TIMESTAMP}/script.tempexe
+rm -rf ${SCRIPTDIR}/Scripts/${TIMESTAMP}
+rm -r ${SCRIPTDIR}/Logs/${TIMESTAMP}
--- generateScript.py
+++ generateScript.py
@@ -1,9 +1,9 @@
 import os, sys, argparse
 
-def GenerateScript(inputFile,inputString):
+def GenerateScript(inputFile,inputString,timestamp):
     commands = 0
     cwd = os.path.dirname(os.path.realpath(__file__))
-    fOut = open(cwd+'/script.tempexe','w')
+    fOut = open('%s/Scripts/%s/script.tempexe' %(cwd,timestamp),'w')
     if inputFile:
         fIn = open(inputFile,'r')
         commands = [line[:-1] if line[-1]=='\n' else line for line in fIn.readlines()]
@@ -12,10 +12,10 @@ def GenerateScript(inputFile,inputString):
     for i,cmd in enumerate(commands):
         startTime = 'export START_TIME=$(date)\n'
         printCmd = 'echo -e "\033[1;33m-> %s \033[0m"\n' %(cmd)
-        wrapCmd = '{ unbuffer time %s ; } 2>&1 | tee -a %s/Logs/status%i.templog\n' %(cmd,cwd,i)
+        wrapCmd = '{ unbuffer time %s ; } 2>&1 | tee -a %s/Logs/%s/status%i.templog\n' %(cmd,cwd,timestamp,i)
         endTime = 'export END_TIME=$(date)\n'
         sleep = 'sleep 1\n'
-        mail = 'echo -e "---> Job completed:\\n%s\\n\\n\\n---> Start time:\\n${START_TIME}\\n---> End time:\\n${END_TIME}\\n\\n\\n---> Job end:\\n$(tail --lines=10 %s/Logs/status%i.templog)\\n\\n\\n---> Full job log:\\n$(less %s/Logs/status%i.templog)" | mutt -s "Job %s (%s of %s) finished" salvatore.porzio@postgrad.manchester.ac.uk\n' %(cmd,cwd,i,cwd,i,cmd,i+1,len(commands))
+        mail = 'echo -e "---> Job completed:\\n%s\\n\\n\\n---> Start time:\\n${START_TIME}\\n---> End time:\\n${END_TIME}\\n\\n\\n---> Job end:\\n$(tail --lines=10 %s/Logs/%s/status%i.templog)\\n\\n\\n---> Full job log:\\n$(less %s/Logs/%s/status%i.templog)" | mutt -s "Job %s (%s of %s) finished" salvatore.porzio@postgrad.manchester.ac.uk\n' %(cmd,cwd,timestamp,i,cwd,timestamp,i,cmd,i+1,len(commands))
         fOut.write(startTime)
         fOut.write(printCmd)
         fOut.write(wrapCmd)
@@ -28,8 +28,9 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-if','--inputFile',required=False,help='Input file',type=str)
     parser.add_argument('-is','--inputString',required=False,help='Input string',type=str)
+    parser.add_argument('-t','--timestamp',required=True,help='Timestamp of request',type=str)
     args = parser.parse_args()
-    GenerateScript(args.inputFile,args.inputString)
+    GenerateScript(args.inputFile,args.inputString,args.timestamp)
 
 if __name__ == '__main__':
     main()
